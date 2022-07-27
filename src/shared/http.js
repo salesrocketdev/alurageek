2c8efe5ff740da5bbe5ad98a5b50de141156dec0
Caio Sales <caiofernandosales@outlook.com>
2022-07-27 03:08:19 -0300

    Update http.js

--- src/shared/http.js
+++ src/shared/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'https://localhost:7006/',
-  //baseURL: 'https://alura-ecommerce.azurewebsites.net/',
+  //baseURL: 'https://localhost:7006/',
+  baseURL: 'https://alura-ecommerce.azurewebsites.net/',
 })
